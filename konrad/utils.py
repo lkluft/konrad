1e87987e1638ddd560e9fe4a6efd838c700bdb2d
Lukas Kluft <lukas.kluft@gmail.com>
2020-07-22 19:24:06 +0200

    Add function to find index of first value below a threshold

--- konrad/utils.py
+++ konrad/utils.py
@@ -30,6 +30,9 @@ __all__ = [
     'prefix_dict_keys',
     'is_decreasing',
     'calculate_combined_weights',
+    'gaussian',
+    'dp_from_dz',
+    'find_first_below',
 ]
 
 logger = logging.getLogger(__name__)
@@ -435,3 +438,39 @@ def calculate_combined_weights(weights):
         pij[i, j] = 1 - is_cloudy + (2 * is_cloudy - 1) * weights[j]
 
     return binary_table, np.prod(pij, axis=1)
+
+
+def gaussian(x, m, s) :
+        """
+        Parameters:
+            x (float): Abscissa
+            m (float): Mean of the gaussian
+            s (float): Standard deviation of the gaussian
+
+        Returns (float) the value of a gaussian distribution of mean m and std s at point x. 
+        """ 
+        X = (x-m)**2 / (2*s**2)
+        return np.exp(-X)
+
+
+def dp_from_dz(dz, p, T):
+    """
+    Obtain the pressure variation dp for an altitude variation dz around the pressure p.
+
+    Parameters:
+        dz (float): altitude variation in m.
+        p (float): pressure at the center of dz in Pa.
+        T (float): temperature at the center of dz in K.
+    """
+
+    dp = ty.physics.density(p, T) * ty.constants.g * dz
+    return dp
+
+
+def find_first_below(arr, val):
+    """Find first index in `arr` that falls below given `val`."""
+    for n, x in np.ndenumerate(arr):
+        if x <= val:
+            return n
+
+    return n
